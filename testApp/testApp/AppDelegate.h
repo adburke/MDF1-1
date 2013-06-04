e8dc1776750a9fdb08e7bac47418a5cdd5717e2d
adburke <adburke@fullsail.edu>
2013-06-04 18:34:34 -0500

    Create main layout

---
+++ testApp/testApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Aaron Burke on 6/4/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testApp/testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Aaron Burke on 6/4/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
