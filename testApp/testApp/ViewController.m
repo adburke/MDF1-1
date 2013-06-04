c79dd28ce076012364f6d0775efa380759b9a710
adburke <adburke@fullsail.edu>
2013-06-04 18:53:13 -0500

    Start configuring main view table

--- testApp/testApp/ViewController.m
+++ testApp/testApp/ViewController.m
@@ -14,6 +14,25 @@
 
 @implementation ViewController
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = @"Testing";
+    
+    return cell;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
